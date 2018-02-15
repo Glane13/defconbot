66217b854ed0057f178aaa5279df964859a71ad7
JonasMogilevec <jonas.mogilevec@online.liverpool.ac.uk>
2018-02-15 21:31:08 +0000

    Added basic functionality for exception handler.

--- src/ExceptionHandler.java
+++ src/ExceptionHandler.java
@@ -1,5 +1,43 @@
+import com.sun.javaws.exceptions.ErrorCodeResponseException;
+
+/**
+ * The ExceptionHandler wraps all checked standard Java exception and enriches them with a custom error code.
+ * You can use this code to retrieve localized error messages.
+ */
+
 public class ExceptionHandler extends Exception{
 
-    // Code goes here
+    private static final long serialVersionUID = 7718828512143293558L;
+
+    private final ErrorCodeResponseException code;
+
+    public ExceptionHandler(ErrorCodeResponseException code) {
+
+        super();
+        this.code = code;
+    }
+
+    public ExceptionHandler(String message, Throwable cause, ErrorCodeResponseException code) {
+
+        super(message, cause);
+        this.code = code;
+    }
+
+    public ExceptionHandler(String message, ErrorCodeResponseException code) {
+
+        super(message);
+        this.code = code;
+    }
+
+    public ExceptionHandler(Throwable cause, ErrorCodeResponseException code) {
+
+        super(cause);
+        this.code = code;
+    }
+
+    public ErrorCodeResponseException getCode() {
+
+        return this.code;
+    }
 
 }
