43a998fe0b3b20e2120adef2360b179e3380cca8
Graham Lane <Glane13@users.noreply.github.com>
2018-02-15 19:48:00 +0000

    ChatBot.java and UserText.java
    The ChatBot object takes user input and passes it to UserText for processing

---
+++ UserText.java
@@ -0,0 +1,7 @@
+package defconbot;
+import javax.swing.JOptionPane;
+public class UserText {
+    public UserText (String userText){
+        JOptionPane.showMessageDialog(null, "This is the input text passed to the UserText class for processing: " + userText);
+    }
+}
