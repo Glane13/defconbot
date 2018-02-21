3804278bac9f9ac2a3ac09656b8f15d0ced9b345
JonasMogilevec <36516059+JonasMogilevec@users.noreply.github.com>
2018-02-21 20:07:05 +0000

    Add files via upload

--- Engine.java
+++ Engine.java
@@ -20,34 +20,35 @@ public class Engine {
         while (itr.hasNext()) {
             Session st = (Session)itr.next();
             if (st.getSpeaker().contains(speaker) && st.getLocation().contains(location) && time == "") {
-                System.out.println("1:: " + st.getSpeaker() + " :: " + st.getLocation() + " :: " + st.getTime() + " :: " + st.getTitle());
+                //System.out.println("1:: " + st.getSpeaker() + " :: " + st.getLocation() + " :: " + st.getTime() + " :: " + st.getTitle());
                 returnList.add(st);
                 //System.out.println(st);
             } else if (st.getLocation().contains(location) && st.getTime().contains(time) && speaker == "") {
-                System.out.println("2:: " + st.getSpeaker() + " :: " + st.getLocation() + " :: " + st.getTime() + " :: " + st.getTitle());
+                //System.out.println("2:: " + st.getSpeaker() + " :: " + st.getLocation() + " :: " + st.getTime() + " :: " + st.getTitle());
                 returnList.add(st);
                 //System.out.println(st);
             } else if (st.getSpeaker().contains(speaker) && st.getTime().contains(time) && location == "") {
-                System.out.println("3:: " + st.getSpeaker() + " :: " + st.getLocation() + " :: " + st.getTime() + " :: " + st.getTitle());
+                //System.out.println("3:: " + st.getSpeaker() + " :: " + st.getLocation() + " :: " + st.getTime() + " :: " + st.getTitle());
                 returnList.add(st);
                 //System.out.println(st);
             } else if (st.getSpeaker().contains(speaker) && location == "" && time == "") {
-                System.out.println("4:: " + st.getSpeaker() + " :: " + st.getLocation() + " :: " + st.getTime() + " :: " + st.getTitle());
+                //System.out.println("4:: " + st.getSpeaker() + " :: " + st.getLocation() + " :: " + st.getTime() + " :: " + st.getTitle());
                 returnList.add(st);
                 //System.out.println(st);
             } else if (st.getLocation().contains(location) && speaker == "" && time == "") {
-                System.out.println("5:: " + st.getSpeaker() + " :: " + st.getLocation() + " :: " + st.getTime() + " :: " + st.getTitle());
+                //System.out.println("5:: " + st.getSpeaker() + " :: " + st.getLocation() + " :: " + st.getTime() + " :: " + st.getTitle());
                 returnList.add(st);
                 //System.out.println(st);
             } else if (st.getTime().contains(time) && speaker == "" && location == "") {
-                System.out.println("6:: " + st.getSpeaker() + " :: " + st.getLocation() + " :: " + st.getTime() + " :: " + st.getTitle());
+                //System.out.println("6:: " + st.getSpeaker() + " :: " + st.getLocation() + " :: " + st.getTime() + " :: " + st.getTitle());
                 returnList.add(st);
                 //System.out.println(st);
             }
 
         }
 
-        System.out.println(returnList);
+        //System.out.println(returnList);
+        return returnList;
     }
 }
 
